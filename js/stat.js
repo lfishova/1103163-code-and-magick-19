27ab5f83131ae3804a69f55a55f8a51a280b1b41
Lena Fishova <matthewfisher1411@gmail.com>
2020-01-24 17:21:06 +0300

    Содержит исправленные замечания наставника

--- js/stat.js
+++ js/stat.js
@@ -12,9 +12,18 @@ var FONT_GAP = 20;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 var FONT_STYLE = '16px PT Mono';
+
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+var renderColumn = function (ctx, str, x, y, h) {
+  ctx.fillStyle = getColorBar(str);
+  ctx.fillRect(x, y, BAR_WIDTH, h);
+};
+var renderText = function (ctx, str, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillText(str, x, y);
 };
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
@@ -37,18 +46,14 @@ window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, SHADOW_COLOR);
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
   ctx.font = FONT_STYLE;
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', CLOUD_X + SMALL_GAP, 4 * SMALL_GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + SMALL_GAP, 6 * SMALL_GAP);
+  renderText(ctx, 'Ура вы победили!', CLOUD_X + SMALL_GAP, 4 * SMALL_GAP, '#000');
+  renderText(ctx, 'Список результатов:', CLOUD_X + SMALL_GAP, 6 * SMALL_GAP, '#000');
   var maxTime = getMaxElement(times);
   var barHeight = 0;
   for (var i = 0; i < players.length; i++) {
     barHeight = (BAR_HEIGHT * times[i]) / maxTime;
-    ctx.fillStyle = '#000';
-    ctx.fillText(players[i], CLOUD_X + GAP + (BAR_WIDTH + GAP) * i, CLOUD_Y + CLOUD_HEIGHT - FONT_GAP);
-    ctx.fillStyle = getColorBar(players[i]);
-    ctx.fillRect(CLOUD_X + GAP + (BAR_WIDTH + GAP) * i, CLOUD_Y + CLOUD_HEIGHT - 4 * SMALL_GAP, BAR_WIDTH, -barHeight);
-    ctx.fillStyle = '#000';
-    ctx.fillText((Math.floor(times[i])).toString(), CLOUD_X + GAP + (BAR_WIDTH + GAP) * i, CLOUD_Y + CLOUD_HEIGHT - barHeight - 2.5 * FONT_GAP);
+    renderText(ctx, players[i], CLOUD_X + GAP + (BAR_WIDTH + GAP) * i, CLOUD_Y + CLOUD_HEIGHT - FONT_GAP, '#000');
+    renderColumn(ctx, players[i], CLOUD_X + GAP + (BAR_WIDTH + GAP) * i, CLOUD_Y + CLOUD_HEIGHT - 4 * SMALL_GAP, -barHeight);
+    renderText(ctx, (Math.floor(times[i])).toString(), CLOUD_X + GAP + (BAR_WIDTH + GAP) * i, CLOUD_Y + CLOUD_HEIGHT - barHeight - 2.5 * FONT_GAP, '#000');
   }
 };
