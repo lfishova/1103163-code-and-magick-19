9dcee9f18497e30f2fc2ae29e97d7c64ddf2aa74
Lena Fishova <matthewfisher1411@gmail.com>
2020-01-26 17:19:42 +0300

    Окончательный вариант на проверку наставнику

--- js/setup.js
+++ js/setup.js
@@ -28,21 +28,27 @@ var renderPerson = function (arrNames, arrSurnames, arrColorCoats, arrColorEyes)
   };
   return person;
 };
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
 var showSetup = function () {
   document.querySelector('.setup').classList.remove('hidden');
 };
 var showSetupSimilar = function () {
   document.querySelector('.setup-similar').classList.remove('hidden');
 };
+
 showSetup();
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var wizards = renderHeroArray(countWizards);
+var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizards.length; i++) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
-  similarListElement.appendChild(wizardElement);
+  fragment.appendChild(renderWizard(wizards[i]));
 }
+similarListElement.appendChild(fragment);
 showSetupSimilar();
