e26da6bd913794fd0b432fb3ce3fcfcbf76e2f27
Lena Fishova <matthewfisher1411@gmail.com>
2020-01-26 16:57:50 +0300

    Добавляет цвета плащей и глаз

--- js/setup.js
+++ js/setup.js
@@ -2,11 +2,10 @@
 
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var colorCoats = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var colorCoats = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var colorEyes = ['black', 'red', 'blue', 'yellow', 'green'];
 var randomValue = Math.floor(Math.random() * 2);
 var countWizards = 4;
-// var wizards = renderHeroArray(countWizards);
 
 var getRandomOrder = function () {
   return randomValue ? getRandomValue(names) + ' ' + getRandomValue(surnames) : getRandomValue(surnames) + ' ' + getRandomValue(names);
@@ -35,15 +34,15 @@ var showSetup = function () {
 var showSetupSimilar = function () {
   document.querySelector('.setup-similar').classList.remove('hidden');
 };
-
 showSetup();
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var wizards = renderHeroArray(countWizards);
 for (var i = 0; i < wizards.length; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
   similarListElement.appendChild(wizardElement);
 }
-
-
 showSetupSimilar();
